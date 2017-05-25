b2c30bb64dc6aa609ffbfbff7d8aaa182c3e1abb
eunice <eunice29548164@gmail.com>
2017-05-25 11:12:26 +0800

    signup

---
+++ signin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>登入</title>
+	<link rel="stylesheet" type="text/css" href="sign.css">
+</head>
+<body>
+	<div class="header">
+		<a>返回首頁</a>
+	</div>
+	<div class="content">
+		<div class="logo">
+			<img src="./pic/logo.png">
+		</div>
+		<div class="signin">
+			<p id="title">會員登入</p>
+			<p id="idd">帳號：<input type="text" name="id"></p>
+			<p id="pwdd">密碼：<input type="password" name="pwd"></p>
+			<button class="upp">註冊</button>
+			<button id="login">登入</button>
+		</div>
+		<div class="clear"></div>
+	</div>
+</body>
+</html>
