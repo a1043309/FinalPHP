43754bec4ab593f4977f2a051a414467eb8380e0
a1043309 <yoyowen1114@gmail.com>
2017-05-27 12:04:02 +0800

    Add files via upload

---
+++ success.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>註冊成功</title>
+	<link rel="stylesheet" type="text/css" href="yoyo.css">
+</head>
+<body>
+	<div class="success_wrap">
+		<div class="success_header">
+			<img src="logo.png">
+		</div>
+		<div class="success_line"></div>
+		<div class="success_content">
+			<img src="eyes.png">
+			<p id="success">&nbsp;&nbsp;&nbsp;恭喜您，註冊成功！！</p>
+			<p id="success_describe">3秒後，自動跳回首頁......</p>
+		</div>
+		<div class="footer">
+			<p>©copyright by 2017 9487DB&PHP</p>
+		</div>
+	</div>
+</body>
+</html>
