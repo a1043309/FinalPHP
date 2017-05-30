dd983d476433ca1b95fb9e5c2189797f35694727
sddd1234567 <sddd1234567@gmail.com>
2017-05-30 20:40:17 +0800

    FUCK

--- index.php
+++ index.php
@@ -215,7 +215,7 @@ $Link = mysqli_connect('localhost','phpholyshit','tingting123','9487');
 					<img src="pic/14248488949068911.jpeg" title="新楓之谷" alt="新楓之谷">
 				</div>
 				<div class="main-data">
-					<? $sql = "SELECT P_NAME,P_Code FROM product WHERE P_Game='maplestory' AND P_Inv > 0";
+					<?php $sql = "SELECT P_NAME,P_Code FROM product WHERE P_Game='maplestory' AND P_Inv > 0";
 						$result = mysqli_query($Link,$sql);
 						for ($i=0; $i < 5; $i++) { 
 							if ($row = mysqli_fetch_assoc($result)) {
@@ -318,7 +318,7 @@ $Link = mysqli_connect('localhost','phpholyshit','tingting123','9487');
 					<img src="pic/23.jpeg" title="爆爆王" alt="爆爆王">
 				</div>
 				<div class="main-data">
-				<?	
+				<?php	
 
 					$sql = "SELECT P_NAME,P_Code FROM product WHERE P_Game='bnb' AND P_Inv > 0";
 					$result = mysqli_query($Link,$sql);
